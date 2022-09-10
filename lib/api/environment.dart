486d77a87e72d22bad22076dba22bc903730ed63
JorgePrez <jorge_operez@yahoo.com>
2022-09-10 04:11:28 -0600

    cambios en onboard

--- lib/api/environment.dart
+++ lib/api/environment.dart
@@ -1,6 +1,7 @@
 class Enviroment {
   static const String API_PARKIATE_KI =
-      "192.168.1.2:3000"; //mi ip... pjuede cambiar , git bash ipconfig
+      "192.168.1.15:3000"; //mi ip... pjuede cambiar , git bash ipconfig
+  //mi ip... pjuede cambiar , git bash ipconfig
 
-  static const String API_PARKIATE_KI2 = "parkiateki.ngrok.io"; //
+  static const String API_PARKIATE_KI2 = "3618-186-151-158-16.ngrok.io"; //
 }
--- lib/languages/spanish.dart
+++ lib/languages/spanish.dart
@@ -1,16 +1,16 @@
 class Spanish {
-
   //Inicio
   static String appName = 'Parkiate-ki';
 
-  static const String titulo1 = 'Administrar fácilmente el estado de tu parqueo';
-  static const String subtitulo1 = 'Podrás ver que espacios estan ocupados y cuales estan libres';
-  static const String titulo2 = 'Implementa reservas';
-  static const String subtitulo2 = 'Elige que espacio destinaras para reservas y administralas';
-  static const String titulo3 = 'Modernizaté haciendo el uso de los códigos QR  ';
-  static const String subtitulo3 = 'Con el uso de QR puedes controlar cuando un auto viene y cuando se va';
+  static const String titulo1 =
+      'Administrar fácilmente el estado de tu parqueo';
+  static const String subtitulo1 =
+      'Podrás ver cúales espacios están ocupados y cuáles están libres';
+  static const String titulo2 = 'Ver qué autos entran y salen';
+  static const String subtitulo2 =
+      'Con el reconocimiento de placas podrás ver a qué hora entra y sale un auto de tu parqueo';
+  static const String titulo3 = 'Modernizaté haciendo el uso de los códigos QR';
+  static const String subtitulo3 =
+      'Con el uso de códigos QR los usuarios pueden registrar qué usaron tu parqueo';
   static String comenzar = 'Comenzar';
-
-
-
 }
